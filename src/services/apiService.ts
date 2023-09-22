78a244e643e7463670f3d59b3b0d01c8a5cfbd9e
Josue Roque <josueroque@yahoo.com>
2023-09-22 02:47:51 -0600

    Clean code

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -55,16 +55,3 @@ export const getPositions = (token: string) => {
     },
   });
 };
-
-// export const getPositions = () => {
-//   const positions = [
-//     { Id: 1, Name: "Conserje", DepartmentId: 1 },
-//     { Id: 2, Name: "Desarrollador Senior", DepartmentId: 2 },
-//     { Id: 3, Name: "Desarrollador Junior", DepartmentId: 2 },
-//     { Id: 4, Name: "Jefe de Recursos Humanos", DepartmentId: 7 },
-//     { Id: 5, Name: "Asistente de Recursos Humanos", DepartmentId: 7 },
-//     { Id: 6, Name: "Auxiliar de Recursos Humanos", DepartmentId: 7 },
-//     { Id: 7, Name: "Jefe de Marketing", DepartmentId: 5 },
-//   ];
-//   return positions;
-// };
